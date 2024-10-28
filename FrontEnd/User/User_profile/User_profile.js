fb7567e4ecd400c30548562ceeb4b4d7c06f7002
ut02010 <ut02010@UNICOMTIC28>
2024-10-28 13:31:04 +0530

    change password javascript

--- FrontEnd/User/User_profile/User_profile.js
+++ FrontEnd/User/User_profile/User_profile.js
@@ -17,6 +17,8 @@ const contactNo = document.getElementById("contactNo");
 const email = document.getElementById("email");
 const address = document.getElementById("address");
 const nicNumber=document.getElementById("nicNumber");
+const changePassword=document.getElementById("changePassword");
+
 
 const today = new Date();
 // if(today>personalInfo.nxtDueDate){
@@ -135,4 +137,65 @@ async function UserEditModal() {
 }
 function closeEditModal() {
     editModal.style.display = 'none'
+}
+
+function changePasswordButton(){
+    changePasswordModal.style.display= 'block'
+}
+changePassword.addEventListener('submit',async function (event){
+    event.preventDefault()
+    const oldPassword = document.getElementById("oldPassword").value
+    const newPassword = document.getElementById("newPassword").value
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    const message=document.getElementById('message');
+    const encryptedPassword=encryptPassword(oldPassword);
+    const res = await fetch(`http://localhost:5237/api/User/Get-User-By-Id/${UserId}`);
+    const User = await res.json();
+
+    if(encryptedPassword==User.password){
+        if(newPassword===confirmPassword){
+            User.password=encryptPassword(newPassword)
+            await fetch(`http://localhost:5237/api/Member/Update-Member/${UserId}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(User)
+            }); 
+        }else{
+            message.innerHTML="confirm password is invalid"
+        }
+    }else{
+        message.innerHTML="Password incorrect"}
+    }
+)
+function encryptPassword(password) {
+    let EncryptPassword = [];
+    //Reversing the password and returning an array
+    for (let i = 0; i < password.length; i++) {
+        EncryptPassword.push(password[(password.length - 1) - i])
+    }
+    for (let i = 0; i < EncryptPassword.length; i = i + 2) {
+        //Changing the Numbers in the password
+        if (!isNaN(EncryptPassword[i])) {
+            if (EncryptPassword[i] <= 4) {
+                EncryptPassword[i] = (EncryptPassword[i]) + 5;
+            } else {
+                EncryptPassword[i] = (EncryptPassword[i]) - 3;
+            }
+        }
+        if (!isNaN(EncryptPassword[i + 1])) {
+            if (EncryptPassword[i + 1] <= 4) {
+                EncryptPassword[i + 1] = (EncryptPassword[i + 1]) + 1;
+            } else {
+                EncryptPassword[i + 1] = (EncryptPassword[i + 1]) - 5;
+            }
+        }
+        //jumbling the characters
+        let temp = EncryptPassword[i];
+        EncryptPassword[i] = EncryptPassword[i + 1];
+        EncryptPassword[i + 1] = temp;
+    }
+    let pasword = EncryptPassword.join("");
+    return pasword;
 }
