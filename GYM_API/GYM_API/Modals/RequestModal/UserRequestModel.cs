d97e832151d8d3e4ab286ff3dc3e72c4b39cb59e
Sacsitha <sacsitha@gmail.com>
2024-10-30 01:37:37 +0530

    reomve files

--- GYM_API/GYM_API/Modals/RequestModal/UserRequestModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GYM_API.Modals.RequestModal
-{
-    public class UserRequestModel
-    {
-        public string Id { get; set; }
-        public string userRole { get; set; }
-        public string password { get; set; }
-    }
-}
--- GYM_API/GYM_API/Modals/ResponseModal/UserResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GYM_API.Modals.ResponseModal
-{
-    public class UserResponseModel
-    {
-        public string Id { get; set; }
-        public string userRole { get; set; }
-        public string password { get; set; }
-    }
-}
